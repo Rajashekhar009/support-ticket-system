116ad5c20b27967a85d23d086c85eeb838a4a6d6
Rajashekhar009 <42096818+Rajashekhar009@users.noreply.github.com>
2025-04-10 15:41:50 +0200

    Update web.php
    Added one extra line to redirect to the Home page

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get("/", [AuthController::class,"login"]);
 
 Route::group(['middleware'=> ['auth']], function () {
 
